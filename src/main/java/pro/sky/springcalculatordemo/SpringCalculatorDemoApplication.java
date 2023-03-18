ee4e45ffa58126280476ab7469cde301558ce38e
Elena Koroleva <koroliana@yandex.ru>
2023-03-18 16:11:46 +0700

    initial commit

---
+++ src/main/java/pro/sky/springcalculatordemo/SpringCalculatorDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springcalculatordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCalculatorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCalculatorDemoApplication.class, args);
+	}
+
+}
