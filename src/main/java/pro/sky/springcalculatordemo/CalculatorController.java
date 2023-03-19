eac477b14c7369a256e320012d49b617eeeb35de
Elena Koroleva <koroliana@yandex.ru>
2023-03-19 16:40:12 +0700

    isNull method added

--- src/main/java/pro/sky/springcalculatordemo/CalculatorController.java
+++ src/main/java/pro/sky/springcalculatordemo/CalculatorController.java
@@ -11,6 +11,11 @@ public class CalculatorController {
         this.calculatorService = calculatorService;
     }
 
+    private boolean isNull(String a, String b){
+        return a == null || b == null;
+    }
+
+
     @GetMapping (path = "/calculator")
     public String greet() {
         return calculatorService.hello();
@@ -18,7 +23,7 @@ public class CalculatorController {
 
     @GetMapping (path = "/calculator/plus")
     public String summarise(@RequestParam(required = false) String num1, @RequestParam(required = false) String num2) {
-        if(num1==null || num2==null ) {
+        if(isNull(num1,num2)) {
             return "Для сложения необходимы 2 числа";
         }
         return calculatorService.plus(num1,num2);
@@ -26,7 +31,7 @@ public class CalculatorController {
 
     @GetMapping (path = "/calculator/minus")
     public String subtraction(@RequestParam(required = false) String num1, @RequestParam(required = false) String num2) {
-        if(num1==null || num2==null ) {
+        if(isNull(num1,num2)) {
             return "Для вычитания необходимы 2 числа";
         }
         return calculatorService.minus(num1,num2);
@@ -34,7 +39,7 @@ public class CalculatorController {
 
     @GetMapping (path = "/calculator/multiply")
     public String multiply(@RequestParam(required = false) String num1, @RequestParam(required = false) String num2) {
-        if(num1==null || num2==null ) {
+        if(isNull(num1,num2)) {
             return "Для умножения необходимы 2 числа";
         }
         return calculatorService.multiply(num1,num2);
@@ -42,7 +47,7 @@ public class CalculatorController {
 
     @GetMapping (path = "/calculator/divide")
     public String divide(@RequestParam(required = false) String num1, @RequestParam(required = false) String num2) {
-        if(num1==null || num2==null ) {
+        if(isNull(num1,num2)) {
             return "Для деления необходимы 2 числа";
         }
         return calculatorService.divide(num1,num2);
